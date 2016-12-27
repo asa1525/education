f47ce92a3828e204c35c606ef7f0d896ae9d8d6f
asa1525 <a28458498@gmail.com>
2016-12-27 18:05:59 +0800

    wrote a php folder

---
+++ new/index.php
@@ -0,0 +1,58 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+    <title>Education online</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" id="dashicons-css" href="css/style.min.css" type="text/css">
+	<script src="https://cdn.static.runoob.com/libs/jquery/2.1.1/jquery.min.js"></script>
+	<script src="https://cdn.static.runoob.com/libs/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+	<div class="head-bar">
+	<div class="container ">
+
+	<div style="margin-left: 0px;" class="">
+	<div style="width: 12%; float: left;">
+	<a href="http://localhost/new/"><img style="vertical-align: middle;padding-top:5px" src="img/logo.png"></a></div>
+	<div style="float: left; width: 80%; margin-top: 28px;">
+	<a style="color: rgb(255, 255, 255) ! important; font-size: 28px; font-family: &quot;微软雅黑&quot;; padding-left: 18px;" href="http://localhost/new/" title="你30岁前的结婚路">thinkphp</a> </div> </div>
+	</div>
+	</div>
+<div class="clearfix"></div>
+  <div class="main-nav">
+   <div style="margin-bottom:0;" class="navbar navbar-inverse">
+    <div class="navbar-inner">
+     <div class="container">
+				<ul class="hidden-phone dxd-navbar-left-items nav" id="yw8">
+				 <li class="active">
+				   <a href="http://localhost/new/">首页</a>
+				 </li>
+				 <li>
+				   <a href="http://localhost/new/">成长课</a>
+				 </li>
+				 <li>
+				   <a href="http://localhost/new/">成长</a>
+				 </li>
+				 <li><a href="http://localhost/new/">成长点拨</a>
+				 </li>
+				</ul>
+				<ul class="pull-right hidden-phone dxd-navbar-right-items nav" id="yw9">
+				 <li>
+				   <a href="http://localhost/new/">注册</a>
+				 </li>
+				 <li>
+				   <a href="http://localhost/new/">登录</a>
+				 </li>
+				</ul>
+      </div>
+     </div>
+    </div>
+</div>
+
+
+</nav>
+
+</body>
+</html>
